782efd846dc4b2e15dfe5973799b72683ee00e72
Recursif <jillian.dum@gmail.com>
2018-04-02 13:31:13 +0200

    express 3

--- node/expressworks/pug/pug.js
+++ node/expressworks/pug/pug.js
@@ -1,16 +1,9 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const pug = require('pug')
+const express = require('express');
+const app = express();
 
-
-app.use(express.static(process.argv[3]))
-app.set('view engine', pug)
-
-app.set('views', path.join(__dirname, 'templates'))
-
-app.get('/home', function(req, res) {
-	res.render('index', {date: new Date().toDateString()})
-})
-app.listen(process.argv[2])
+app.set('views', process.argv[3]);
+app.set('view engine', 'pug');
+app.get('/home', function(req, res) {	
+	res.render('index', {date: new Date().toDateString()});
+}).listen(process.argv[2])
 
