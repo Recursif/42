c9c943759ae8d55f487aa371303def37a638e6d5
Recursif <jillian.dum@gmail.com>
2018-05-04 09:53:23 +0200

    db

--- node/learnyoumongo/find-project/find-project.js
+++ node/learnyoumongo/find-project/find-project.js
@@ -6,7 +6,6 @@ const url = 'mongodb://localhost:27017/learnyoumongo';
 mongo.connect(url, function(err, db) {
 	if (err) throw err
 	const parrots = db.collection('parrots');
-	
 	parrots.find({
 		age: {$gt: age}
 		}, { age: 1,
--- node/learnyoumongo/update/update.js
+++ node/learnyoumongo/update/update.js
@@ -1,21 +1,18 @@
-const mongo = require('mongodb').MongoClient
+var mongo = require('mongodb').MongoClient
 
-const url = 'mongodb://localhost:27017/' + process.argv[2]
-
-mongo.connect(url, fonction(err, db) {
-	if (err) console.log(err)
-	
-	const users = db.collection('users')
-	
-	users.update({
-		username: 'tinatime'
-	}, {
-		$set: {
-			age: 40
-		}
-	}, function(err) {
-		if (err) console.log(err)
-		db.close()
-	})
+var url = 'mongodb://localhost:27017/' + process.argv[2]
+mongo.connect(url, function(err, db) {
+  if (err) console.log(err)
+  console.log(db);
+  var collection = db.collection('users')
+  collection.update({
+    username: 'tinatime'
+  }, {
+    $set: {
+      age: 40
+    }
+  }, function(err) {
+    if (err) console.log(err)
+    db.close()
+  })
 })
-
