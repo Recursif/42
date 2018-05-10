f279e7ae4b72eceda9d682b91b0de91db31fa592
Recursif <jillian.dum@gmail.com>
2018-05-10 15:50:18 +0200

    router ok

--- node/router-dom/react-router-example/src/App.js
+++ node/router-dom/react-router-example/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './component/Home';
+import About from './component/About';
+import Messages from './component/Messages';
+
+import { Link, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +16,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="menu">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/messages">Messages</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+        <div className="App-intro">
+          <Route exact path="/" component={Home}/>
+          <Route path="/messages" component={Messages}/>
+          <Route path="/about" component={About}/>
+        </div>
       </div>
     );
   }
---
+++ node/router-dom/react-router-example/src/component/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Messages = () => (
+  <div>
+    <h2>Messages</h2>
+    ok
+  </div>
+);
+
+export default Messages;
