83524a1f7d12248a46c519aa7520cd7a743059f1
Recursif <jillian.dum@gmail.com>
2018-05-10 16:32:22 +0200

    nested routes

--- node/router-dom/react-router-example/src/component/Messages.js
+++ node/router-dom/react-router-example/src/component/Messages.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Messages = () => (
   <div>
     <h2>Messages</h2>
-    ok
+    <ul>
+    {
+      [...Array(5).keys()].map(n => {
+        return <li key={n}>
+          <Link to={`${match.url}/${n+1}`}>
+            Messages {n+1}
+          </Link>
+          </li>;
+      })
+    }
+    </ul>
+    <Route path={`{match.url}/:id`} component={Message} />
   </div>
 );
 
