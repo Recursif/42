1dcf00c64e5433e921583ea1a8300f7b7778561e
Recursif <jillian.dum@gmail.com>
2018-05-12 23:29:50 +0200

    tic

--- react/tic-tac-toe/src/App.js
+++ react/tic-tac-toe/src/App.js
@@ -11,8 +11,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To g
+
+
+          et started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Block/>
+        <Block/>
+        <Block/>
       </div>
     );
   }
