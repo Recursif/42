f065bb14fce2a55fe4aae0ecdca9c65a803fe0d0
Recursif <jillian.dum@gmail.com>
2018-05-13 12:29:40 +0200

    server-rendering

---
+++ react/server-rendering/src/browser/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { hydrate } from 'react-dom'
+import App from '../shared/App'
+
+hydrate(
+  <App/>
+  document.getElementByID('app')
+)
+
+
+/*
+
+This will adding event listeners and interactivity
+
+
+*/
