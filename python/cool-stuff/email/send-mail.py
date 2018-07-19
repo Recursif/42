0dcf40cd17066d113c1d4fa6e83634991559b52b
Recursif <jillian.dum@gmail.com>
2018-07-19 09:09:19 +0200

    add some feature with mailing in python

---
+++ python/cool-stuff/email/send-mail.py
@@ -0,0 +1,22 @@
+
+import smtplib 
+from email.MIMEMultipart import MIMEMultipart 
+from email.MIMEText import MIMEText
+
+msg = MIMEMultipart()
+msg['From'] = 'XXX@gmail.com'
+msg['To'] = 'YYY@gmail.com'
+msg['Subject'] = 'Le sujet de mon mail'
+
+message = 'Bonjour !'
+
+msg.attach(MIMEText(messages))
+
+mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+mailserver.ehlo()
+mailserver.starttls()
+mailserver.ehlo()
+mailserver.login('XX@gmail.com', 'PASSWORD')
+mailserver.sendmail('XXX@gmail.com', 'XXX@gmail.com', msg.as_string())
+mailserver.quit()
+
